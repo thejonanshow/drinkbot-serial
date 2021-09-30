b77f2ab36f5f0f2fd31e2ac00bb1d9028a98880a
Aaron Bassett <me@aaronbassett.com>
2021-09-29 19:34:34 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,123 @@
+import redis
+import json
+import serial
+import sys
+import time
+import random
+import string
+from serial import SerialException
+import newrelic.agent
+
+from multiprocessing import Process
+
+newrelic.agent.initialize()
+newrelic.agent.register_application()
+application = newrelic.agent.application()
+
+
+class DrinkbotSerial:
+
+    redis_conn = redis.Redis(charset="utf-8", decode_responses=True)
+    usbport = "/dev/ttyAMA0"
+    name = None
+
+    def __init__(self):
+        try:
+            self.serial = serial.Serial(self.usbport, 9600, timeout=0)
+        except serial.SerialException as e:
+            print("Error, ", e)
+            sys.exit(0)
+
+        self._init_name()
+
+    def _read_line(self):
+        lsl = len(b"\r")
+        line_buffer = []
+        while True:
+            next_char = self.serial.read(1)
+            if next_char == b"":
+                break
+            line_buffer.append(next_char)
+            if len(line_buffer) >= lsl and line_buffer[-lsl:] == [b"\r"]:
+                break
+        return b"".join(line_buffer)
+
+    def _read_lines(self):
+        lines = []
+        try:
+            while True:
+                line = self.read_line()
+                if not line:
+                    break
+                self.serial.flush_input()
+                lines.append(line)
+            return lines
+
+        except SerialException as e:
+            print("Error, ", e)
+            return None
+
+    def _init_name(self):
+        # Check for existing name, we wait after sending command
+        # before checking for response
+        self.send_cmd("Name,?")
+        time.sleep(2)
+        response = self._read_lines()
+
+        # Loop over responses and check for a name response
+        for line in response:
+            if line[:6] == "?Name,":
+                command, name = line.split(",")
+                if name != "":
+                    self.name = name
+
+        # No name set yet, so no response of response was blank
+        # Generate a random name and assign
+        if self.name == None:
+            self.name = "".join(
+                random.choices(string.ascii_letters + string.digits, k=16)
+            )
+            self.send_cmd(f"Name,{self.name}")
+
+            # wait before we return that we're ready to accept commands
+            time.sleep(2)
+
+        # Publish our new/current name
+        self.redis_conn.publish("drinkbot", json.dumps({"name": self.name}))
+
+        return self.name
+
+    def send_cmd(self, cmd):
+        buf = cmd + "\r"  # add carriage return
+
+        try:
+            self.serial.write(buf.encode("utf-8"))
+            return True
+        except SerialException as e:
+            print("Error, ", e)
+            return None
+
+    def listen_for_commands(self):
+        pubsub = self.redis_conn.pubsub()
+        pubsub.subscribe("drinkbot")
+
+        for message in pubsub.listen():
+            if message.get("type") == "message":
+                data = json.loads(message.get("data"))
+
+                if "name" in data and "command" in data and data["name"] == self.name:
+                    if data["command"][:5] == "Name,":
+                        self.name = data["command"].split(",")[1]
+                    elif data["command"][:2] == "D,":
+                        # Dispensing, add metric to New Relic
+                        newrelic.agent.record_custom_metric(
+                            f"{self.name}/Dispensed",
+                            data["command"].split(",")[1],
+                            application,
+                        )
+                    self.send_cmd(data["command"])
+
+
+if __name__ == "__main__":
+    dbs = DrinkbotSerial()
+    Process(target=dbs.listen_for_commands).start()
